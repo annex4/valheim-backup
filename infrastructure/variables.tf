cf38a2554e2e25da8813447a2727a83578f188a4
Ryan Boucher <ryan-boucher@hotmail.com>
2021-03-02 10:57:06 -0500

    Added infrastructure

---
+++ infrastructure/variables.tf
@@ -0,0 +1,28 @@
+variable "server_id" {
+    type = string
+}
+
+variable "api_key" {
+    type = string
+}
+
+variable "host" {
+    type = string
+}
+
+variable "bucket" {
+    type = string
+}
+
+variable "world" {
+    type = string
+}
+
+variable "region" {
+    type = string
+    default = "us-east-1"
+}
+
+variable "account" {
+    type = string
+}
